08bed37ff8f60fd82672fa1512a4e63a4c429201
CHENHONGHONG\宏鸿 <aspwebchh@hotmail.com>
2018-01-25 09:41:56 +0800

    response length长度设置bug修复

--- src/web/BaseHandler.java
+++ src/web/BaseHandler.java
@@ -3,6 +3,7 @@ package web;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import common.Common;
+import common.ExecResult;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
@@ -24,8 +25,11 @@ public abstract class BaseHandler implements HttpHandler {
                 String postDataString = IOUtils.toString(exchange.getRequestBody());
                 Map<String,String> postData = Common.formData2Dic(postDataString);
                 response = handle(postData);
+            } else {
+                ExecResult result = ExecResult.with(ExecResult.CODE_ERROR,"不支持非POST请求");
+                response = result.toJSON();
             }
-            exchange.sendResponseHeaders(200, response.length());
+            exchange.sendResponseHeaders(200,0);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();

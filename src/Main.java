f399ae5d579221ffa148ef057561c9eea775240a
CHENHONGHONG\宏鸿 <aspwebchh@hotmail.com>
2018-02-06 09:37:31 +0800

    配置文件内容更新

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,15 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
+
+import common.Config;
 import web.*;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        Config config = Config.fromFile();
+        HttpServer server = HttpServer.create(new InetSocketAddress(config.getServerPort()), 0);
         server.createContext("/create", new CreateWordHandler());
         server.createContext("/find", new FindWordHandler());
         server.createContext("/delete", new DelWordHandler());
--- src/common/Config.java
+++ src/common/Config.java
@@ -4,20 +4,26 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.io.IOUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class Config {
     private final String databaseUrl;
     private final String databaseUser;
     private final String databasePassword;
+    private final String serverPort;
 
-    public Config( String databaseUrl, String databaseUser, String databasePassword) {
+    public Config( String databaseUrl, String databaseUser, String databasePassword, String serverPort) {
         this.databaseUrl = databaseUrl;
         this.databaseUser = databaseUser;
         this.databasePassword = databasePassword;
+        this.serverPort = serverPort;
+    }
+
+    public int getServerPort() throws InvalidObjectException {
+        if(!Common.isInteger(this.serverPort)) {
+            throw new InvalidObjectException("端口号格式错误");
+        }
+        return Integer.parseInt( serverPort );
     }
 
     public String getDatabaseUrl() {
@@ -53,10 +59,11 @@ public class Config {
             final String databaseUrl = configData.getString("database_url");
             final String databaseUser = configData.getString("database_user");
             final String databasePassword  = configData.getString("database_password");
-            Config config = new Config(databaseUrl,databaseUser,databasePassword);
+            final String serverPort = configData.getString("server_port");
+            Config config = new Config(databaseUrl,databaseUser,databasePassword, serverPort);
             return config;
         } catch (Exception e) {
-            return new Config("","","");
+            return new Config("","","","");
         }
     }
 }
